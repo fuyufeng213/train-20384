e4c19e573e3f87844a296a24a46a85d817f88871
FUYUFENG <YUFENG.FU@HAND-CHINA.COM>
2018-08-18 13:17:35 +0800

    20384猪齿鱼考核

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # train-20384
 猪齿鱼框架考试作业
+# train-20384
+
+This is a choerodon microservice template.
+
---
+++ src/main/java/io/choerodon/demo/api/controller/v1/DemoController.java
@@ -0,0 +1,31 @@
+package io.choerodon.demo.api.controller.v1;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.choerodon.core.iam.ResourceLevel;
+import io.choerodon.swagger.annotation.Permission;
+
+@RestController
+@RequestMapping("v1/demo")
+public class DemoController {
+
+    @Value("${choerodontest.username}")
+    private String username;
+    @Value("${choerodontest.number}")
+    private String number;
+
+    @GetMapping("/hello")
+    @Permission(level = ResourceLevel.SITE, permissionPublic = true)
+    @ApiOperation(value = "demo")
+    public ResponseEntity<String> hello() {
+
+        return new ResponseEntity<String>("hello,"+number+username, HttpStatus.OK);
+    }
+
+}
